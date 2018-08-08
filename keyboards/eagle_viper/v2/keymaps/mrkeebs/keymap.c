9cfa1a6fdbe07e4f84f48ecf5810bc1dcee9561b
Felipe Coury <felipe.coury@gmail.com>
2018-08-07 23:01:07 -0300

    feat(eagle_viper:v2:mrkeebs): added CAPS to second layer

--- keyboards/eagle_viper/v2/keymaps/mrkeebs/keymap.c
+++ keyboards/eagle_viper/v2/keymaps/mrkeebs/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [1] = LAYOUT_all(\
         KC_GRV,  KC_F1,   KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,  KC_F12,  ______,  KC_DEL,
-        ______,  RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_M_SW, ______, KC_UP,   ______,  RESET,
+        KC_CAPS,  RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_M_SW, ______, KC_UP,   ______,  RESET,
         ______,  KC_MUTE, KC_VOLD, KC_VOLU, ______, ______, ______, ______, ______, ______,  KC_LEFT, KC_RIGHT, ______, ______,
         ______, ______, KC_PSCR, KC_SLCK, ______, ______, ______, ______, ______, ______, ______, KC_DOWN, ______, ______,
         ______, ______, ______,                  ______,                                  ______, ______, ______, ______, ______),
