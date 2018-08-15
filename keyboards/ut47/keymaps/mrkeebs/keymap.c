a7e13aad8b3defd0c7f496858f6a0405cee6120f
Felipe Coury <felipe.coury@gmail.com>
2018-08-15 13:44:43 -0300

    feat(ut47:mrkeebs): activated and tweaked Layer 2

--- keyboards/ut47/keymaps/mrkeebs/keymap.c
+++ keyboards/ut47/keymaps/mrkeebs/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 LAYOUT(
   KC_GESC,              KC_Q,    KC_W,    KC_E,    KC_R,          KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-  MT(MOD_LCTL, KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,          KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+  MT(MOD_LCTL, KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,          KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT(2, KC_ENT),
   KC_LSFT,              KC_Z,    KC_X,    KC_C,    KC_V,          KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   MT(MOD_RSFT, KC_SLSH),
   KC_LCTL,              KC_LALT, KC_LGUI, KC_LGUI, LT(1, KC_SPC),      KC_SPC,        KC_SPC,  MO(1),   KC_LEFT, KC_DOWN,   KC_RGHT
 ),
@@ -80,10 +80,10 @@ LAYOUT( /* Right */
    */
 
 LAYOUT( /* Left */
-  KC_TILDE,  KC_EXCLAIM,  KC_AT,  KC_HASH,  KC_DOLLAR, KC_PERCENT, KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_LEFT_PAREN, KC_RIGHT_PAREN, KC_DELETE,
-  _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, KC_UNDERSCORE, KC_PLUS, KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, KC_PIPE,
-  _______, KC_PSCR, KC_LSCR, _______, KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-  _______, _______, _______, KC_CAPS, _______,     _______,      _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, KC_DEL,
+  _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_M_SW, _______, _______,
+  _______, KC_PSCR, KC_LSCR, _______, _______, _______, _______, _______, _______, _______,  _______, RESET,
+  _______, _______, _______, _______, _______,     _______,      _______, _______, _______,  _______, _______
 ),
 
   /* FN Layer 2
