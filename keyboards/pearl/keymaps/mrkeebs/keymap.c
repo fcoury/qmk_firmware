1c3df3fe039147c183fc52c40fe38f565f6df0c6
Felipe Coury <felipe.coury@gmail.com>
2018-08-16 22:40:58 -0300

    feat(pearl:mrkeebs): LEDs light always on except when layers are toggled

--- keyboards/pearl/keymaps/mrkeebs/keymap.c
+++ keyboards/pearl/keymaps/mrkeebs/keymap.c
@@ -46,3 +46,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       ),
 };
 
+uint32_t layer_state_set_kb(uint32_t state) {
+  // if we are on layer 1
+  if (state & (1<<1)){
+    // light num lock led
+    PORTD &= ~(1 << PD0);
+  }
+  else{
+    PORTD |= (1 << PD0);
+  }
+  // if we are on layer 2
+  if (state & (1<<2)){
+    // light caps lock led
+    PORTD &= ~(1 << PD1);
+  }
+  else{
+    PORTD |= (1 << PD1);
+  }
+  // if we are on layer 3
+  if (state & (1<<3)){
+    // light scroll lock led
+    PORTD &= ~(1 << PD6);
+  }
+  else{
+    PORTD |= (1 << PD6);
+  }
+    return state;
+}
