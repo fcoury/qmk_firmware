6fbf220d8ea8a46b61c5033ee5d037f29a924e30
Felipe Coury <felipe.coury@gmail.com>
2018-12-04 23:43:59 -0200

    feat(alps64:mrkeebs): made CAPS into LCTL

--- keyboards/alps64/keymaps/mrkeebs/keymap.c
+++ keyboards/alps64/keymaps/mrkeebs/keymap.c
@@ -5,14 +5,13 @@ enum layers {
   _CTRL
 };
 
-#define KC_CTAB MT(MOD_LCTL, KC_TAB)  // Control held, Tab pressed
-#define KC_CTLR MO(_CTRL)             // Control layer
+#define KC_CTLR MO(_CTRL) // Control layer
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_all( \
     KC_GESC, KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NUHS, KC_BSPC, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
-    KC_CAPS, KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  \
+    KC_LCTL, KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  \
     KC_LSFT, KC_NUBS, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_CTLR, KC_ESC,  \
     KC_LCTL, KC_LGUI, KC_LALT,                 KC_SPC,                                  KC_APP,  KC_RALT, KC_RGUI, KC_RCTL
   ),
@@ -24,3 +23,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______,                 _______,                                       _______,  _______, _______, _______
   ),
 };
+
+
