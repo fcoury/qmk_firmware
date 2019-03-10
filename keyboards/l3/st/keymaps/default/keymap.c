1ffacb054bde21febd4febf84381148d1ebf8c93
Felipe Coury <felipe.coury@gmail.com>
2019-03-10 12:25:31 -0300

    chore(l3:default): initial commit

---
+++ keyboards/l3/st/keymaps/default/keymap.c
@@ -0,0 +1,10 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  KEYMAP(
+      ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, NO,  BSPC,INS,
+      TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,     DEL,
+      FN0, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,NO,  ENT,
+      LSFT,NO,  Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,NO,       UP,
+      LCTL,LGUI,LALT,               SPC,                     FN1, RGUI,RCTL,LEFT,DOWN,RGHT)
+};
