c879c73d52d1c27c9f3dff1e08c25868263aaac2
Felipe Coury <felipe.coury@gmail.com>
2019-05-01 07:38:35 -0300

    feat(unigo66:mrkeebs): backspace on left thumb and quote and grave on right thumb

--- keyboards/sirius/unigo66/keymaps/mrkeebs/keymap.c
+++ keyboards/sirius/unigo66/keymaps/mrkeebs/keymap.c
@@ -28,10 +28,10 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
   KC_ESC,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_MINS,      KC_EQL,  KC_6,   KC_7,   KC_8,   KC_9,   KC_0,    KC_BSLS,
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_PGUP,      KC_LBRC, KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,    KC_BSPC,
   KC_LCTL, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                          KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_ENT,
-  KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_PGDN,      KC_RBRC, KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, MO(2),
-  KC_LALT, KC_LALT,KC_LGUI,                                                                       KC_LEFT,KC_DOWN, KC_SFRT,
-                                           KC_L1SP,KC_L1BS,       KC_HOME, KC_L1SP,
-                                           KC_NO , KC_L1BS,       KC_END,  KC_NO
+  KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_PGDN,      KC_RBRC, KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_SFRT,
+  KC_LALT, KC_LALT,KC_LGUI,                                                                       KC_LEFT,KC_DOWN, MO(2),
+                                           KC_L1SP,KC_L1BS,       KC_GRV, KC_L1SP,
+                                           KC_NO , KC_L1BS,       KC_QUOT,KC_NO
     ),
 /*
  *
