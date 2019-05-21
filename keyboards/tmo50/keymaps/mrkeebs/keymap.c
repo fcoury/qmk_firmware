76dc9de6e0fad288a3056f089276e49ccd17c3fd
Felipe Coury <felipe.coury@gmail.com>
2019-05-21 17:11:30 -0300

    feat(tmo50:mrkeebs): works both with split space and regular TMOs

--- keyboards/tmo50/keymaps/mrkeebs/keymap.c
+++ keyboards/tmo50/keymaps/mrkeebs/keymap.c
@@ -18,8 +18,9 @@
 enum layers {
     _BASE = 0, // Base
     _ALT,      // Alternate
-    _EXTR,     // Extra
+    _MEDIA,    // Media
     _CTRL,     // Control
+    _EXTR,     // Extra
 		_RGB
 };
 
@@ -33,42 +34,50 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Default layer
-  [_BASE] = LAYOUT_space(
+  [_BASE] = LAYOUT_all(
 		KC_HOME, KC_ESC,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
 		KC_PGUP, KC_CTAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_RGEN,
 		KC_PGDN, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(_CTRL),
-		KC_END,  KC_LGUI, KC_LALT,                 KC_EXSP,             KC_LCTL
+		KC_END,  KC_LGUI, KC_LALT,                 KC_EXSP, KC_SPC,            KC_RALT, KC_LCTL
   ),
 
   // Alternate layer
-  [_ALT] = LAYOUT_space(
+  [_ALT] = LAYOUT_all(
 		MC_MUTE, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 		MC_VOLU, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 		MC_VOLD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-		KC_MPLY, _______, _______, _______, _______
+		KC_MPLY, _______, _______, _______, _______, _______, _______
   ),
 
-  // Extra keys layer
-  [_EXTR] = LAYOUT_space(
-		_______, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, _______,
-		_______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PLUS, KC_MINS, _______, _______, _______,
-		_______, _______, _______, _______, _______, KC_SPC,  _______, _______, _______, _______, _______, KC_BSLS, _______, _______,
-		_______, KC_ESC,  _______, _______, _______
+  // Media layer
+  [_MEDIA] = LAYOUT_all(
+		KC_MUTE, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		KC_VOLU, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		KC_VOLD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		KC_MPLY, _______, _______, _______, _______, _______, _______
   ),
 
   // Control layer
-  [_CTRL] = LAYOUT_space(
-		TG(_ALT), _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END,  KC_UP,   _______, KC_DEL,
-		KC_BRID, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, RESET,
-		_______, _______, KC_MPLY, KC_MPRV, KC_MNXT, _______, _______, _______, _______, KC_END,  KC_PGDN, KC_DOWN, _______, _______,
-		_______, _______, _______, _______, _______
+  [_CTRL] = LAYOUT_all(
+		TG(_ALT),   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END,  KC_UP,   _______, KC_DEL,
+		TG(_MEDIA), _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, RESET,
+		_______,    _______, KC_MPLY, KC_MPRV, KC_MNXT, _______, _______, _______, _______, KC_END,  KC_PGDN, KC_DOWN, _______, _______,
+		_______,    _______, _______, _______, _______, _______, _______
+  ),
+
+  // Extra keys layer
+  [_EXTR] = LAYOUT_all(
+		_______, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PLUS, KC_MINS, _______, _______, _______,
+		_______, _______, _______, _______, _______, KC_SPC,  _______, _______, _______, _______, _______, KC_BSLS, _______, _______,
+		_______, KC_ESC,  _______, _______, _______, _______, _______
   ),
 
   // RGB layer
-  [_RGB] = LAYOUT_space(
+  [_RGB] = LAYOUT_all(
     RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
 		_______, KC_CAPS, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, BL_TOGG, BL_INC, _______, _______, _______, _______, _______, _______,
 		_______, _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, BL_STEP, BL_DEC, _______, _______, _______, _______, _______, _______,
-		_______, _______, _______, _______, _______
+		_______, _______, _______, _______, _______, _______, _______
   ),
 };
