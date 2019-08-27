b8793bf9a00bb02ea5360a006b5dab09dd145adc
Felipe Coury <felipe.coury@gmail.com>
2019-08-27 17:59:22 -0300

    feat(dz60:mrkeebs_hhkb_winopt): added WIN toggling as Fn+RGui

--- keyboards/dz60/keymaps/mrkeebs_hhkb_winopt/keymap.c
+++ keyboards/dz60/keymaps/mrkeebs_hhkb_winopt/keymap.c
@@ -8,6 +8,7 @@ enum layers {
 };
 
 #define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
+#define KC_TWIN TG(_WIN)
 
 const uint8_t RGBLED_RAINBOW_SWIRL_INTERVALS[] PROGMEM = {10, 5, 5};
 
@@ -32,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,                KC_Q,      KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
       MT(MOD_LCTL, KC_CAPS), KC_A,      KC_S,    KC_D,   KC_F,   KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,       KC_ENT    ,
       KC_LSFT,               _______,   KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SLSH, MO(_CTRL),
-   		KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_SPC, KC_RGUI, KC_RGUI, KC_RGUI, TG(_WIN), KC_RCTL
+   		KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_SPC, KC_RGUI, KC_RGUI, KC_RGUI, KC_RALT, KC_RCTL
       ),
 
   [_WIN] = LAYOUT(
@@ -40,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, _______,    _______,    _______,   _______,   _______,   _______,    _______,    _______,    _______,    _______,    _______, _______, _______,
       _______, _______,    _______,    _______,   _______,   _______,   _______,    _______,    _______,    _______,    _______, _______,       _______,
       _______, _______,  _______,    _______,   _______,   _______,   _______,    _______,    _______,    _______, _______,  _______, _______, _______,
-   		_______, KC_LGUI, KC_LALT, _______, _______, _______, _______, _______, _______, _______, _______
+   		KC_LCTL, KC_LGUI, KC_LALT, _______, _______, _______, _______, _______, _______, _______, _______
       ),
 
   [_CTRL] = LAYOUT(
@@ -48,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_HOME, KC_END, KC_UP, _______, RESET,
       _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______, _______, KC_PGUP, KC_LEFT, KC_RIGHT, _______,
       _______, _______, KC_PSCR, KC_SLCK, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_DOWN, _______, _______,
-		  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+		  _______, _______, _______, _______, _______, _______, _______, _______, KC_TWIN, _______, _______
       ),
 };
 
