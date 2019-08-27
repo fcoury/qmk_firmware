41c5fd6d2e2b90fb1373d03d0f7187297f668bf3
Felipe Coury <felipe.coury@gmail.com>
2019-08-27 17:47:22 -0300

    feat(dz60:mrkeebs_wkl): added extra keys (HOME, END, PGUP, PGDN)

--- keyboards/dz60/keymaps/mrkeebs_wkl/keymap.c
+++ keyboards/dz60/keymaps/mrkeebs_wkl/keymap.c
@@ -1,8 +1,12 @@
-#include "dz60.h"
+#include QMK_KEYBOARD_H
+
+// Layer shorthand
+enum layers {
+    _BASE = 0, // Base
+    _CTRL      // Extras
+};
 
 #define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
-
-#define ______ KC_TRNS
 
 const uint8_t RGBLED_RAINBOW_SWIRL_INTERVALS[] PROGMEM = {10, 5, 5};
 
@@ -22,34 +26,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------------'
  */
 
-  LAYOUT(
-      KC_GESC,               KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
-      KC_TAB,                KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
-      MT(MOD_LCTL, KC_CAPS), KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,       KC_ENT    ,
-      KC_LSFT,               ______,  KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SLSH, MO(1),
+  [_BASE] = LAYOUT(
+      KC_GESC, KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
+      KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
+      KC_LCTL, KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,       KC_ENT    ,
+      KC_LSFT, _______, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SLSH, MO(_CTRL),
    		KC_LALT, KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_SPC, KC_RGUI, KC_RGUI, KC_RGUI, KC_RALT, KC_LALT
       ),
 
-/* FN Layer
- * ,-----------------------------------------------------------------------------------------.
- * | Esc | BL- | BL+ | BL  |     |     |     |RESET|     |     |     |     |     |           |
- * |-----------------------------------------------------------------------------------------+
- * |        |RBGM |     |     |     |     |     |     |     |     |     | Val+ | Val- |RBGTOG|
- * |-----------------------------------------------------------------------------------------+
- * |         | Hue+| Hue-| Sat+| Sat-|     |     |     |     |     |     |     |             |
- * |-----------------------------------------------------------------------------------------+
- * |           |     |     |     |     |     |     |     |     |     |     |     | Ctrl|     |
- * |-----------------------------------------------------------------------------------------+
- * |      |       |       |                                   |     |      |     |     |     |
- * `-----------------------------------------------------------------------------------------'
- */
-
-  LAYOUT(
-      KC_GRV, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_F11, KC_F12, ______, KC_DEL,
-      ______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_M_SW, ______, KC_UP, ______, RESET,
-      ______, KC_MUTE, KC_VOLD, KC_VOLU, ______, ______, ______, ______, ______, ______, KC_LEFT, KC_RIGHT, ______,
-      ______, ______, KC_PSCR, KC_SLCK, ______, ______, ______, ______, ______, ______, ______, KC_DOWN, ______, ______,
-		  ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______
+  [_CTRL] = LAYOUT(
+      KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12, _______, KC_DEL,
+      _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_HOME, KC_END,  KC_UP,    _______, RESET,
+      _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_LEFT, KC_RIGHT, _______,
+      _______, _______, KC_PSCR, KC_SLCK, _______, _______, _______, _______, _______, KC_END,  KC_PGDN, KC_DOWN,  _______, _______,
+		  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
       ),
 };
 
