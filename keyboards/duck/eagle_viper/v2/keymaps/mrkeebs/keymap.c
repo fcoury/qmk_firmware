2f5aabf159bf857415ef1e5892c2645c4d72fecc
Felipe Coury <felipe.coury@gmail.com>
2019-10-19 18:14:35 -0300

    fix(duck/eagle_viper:mrkeebs): fixed location and added pgup/pgdn/home/end key cluster

--- keyboards/duck/eagle_viper/v2/keymaps/mrkeebs/keymap.c
+++ keyboards/duck/eagle_viper/v2/keymaps/mrkeebs/keymap.c
@@ -24,14 +24,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
         KC_LCTL, KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT, KC_NO,   KC_ENT,
         KC_LSFT, KC_NO,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(1),
-        KC_LCTL, KC_LGUI, KC_LALT,                KC_SPC,                                  MO(1),   KC_NO,   KC_RALT, KC_RGUI, KC_RCTL),
+        KC_LCTL, KC_LGUI, KC_LALT,                KC_SPC,                                  MO(1),   KC_RALT, KC_RGUI, KC_RCTL),
 
     [1] = LAYOUT_all(\
         KC_GRV,  KC_F1,   KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,  KC_F12,  ______,  KC_DEL,
         KC_CAPS,  RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_M_SW, ______, KC_UP,   ______,  RESET,
-        ______,  KC_MUTE, KC_VOLD, KC_VOLU, ______, ______, ______, ______, ______, ______,  KC_LEFT, KC_RIGHT, ______, ______,
-        ______, ______, KC_PSCR, KC_SLCK, ______, ______, ______, ______, ______, ______, ______, KC_DOWN, ______, ______,
-        ______, ______, ______,                  ______,                                  ______, ______, ______, ______, ______),
+        ______,  KC_MUTE, KC_VOLD, KC_VOLU, ______, ______, ______, ______, KC_HOME, KC_PGUP,  KC_LEFT, KC_RIGHT, ______, ______,
+        ______, ______, KC_PSCR, KC_SLCK, ______, ______, ______, ______, ______, KC_END, KC_PGDN, KC_DOWN, ______, ______,
+        ______, ______, ______,                  ______,                                  ______, ______, ______, ______),
         };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
