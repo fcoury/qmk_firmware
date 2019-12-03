2a6e6036296176da0e22d75d6ec875bc784dd360
Felipe Coury <felipe.coury@gmail.com>
2019-12-02 22:05:50 -0300

    fix(gray_studio/hb85:mrkeebs): moved function keys to the right place

--- keyboards/gray_studio/hb85/keymaps/mrkeebs/keymap.c
+++ keyboards/gray_studio/hb85/keymaps/mrkeebs/keymap.c
@@ -30,9 +30,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT_stt(
         RESET,   RGB_M_P,  RGB_M_B,  RGB_M_R,  RGB_M_SW, RGB_M_SN, RGB_M_K,  RGB_M_X,  RGB_M_G,  _______,  _______,  _______, _______, _______,                            \
         EEP_RST, _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______, _______, _______, _______, _______, \
-        _______, KC_MUTE,  KC_VOLU,  KC_VOLD,  RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_SAI,  RGB_VAI,  RGB_SPI,  _______,  _______, _______, _______, KC_HOME, KC_UP  , KC_PGUP, \
-        _______, _______,  _______,  _______,  _______,  RGB_RMOD, RGB_HUD,  RGB_SAD,  RGB_VAD,  RGB_SPD,            _______, _______, _______, KC_LEFT, _______, KC_RGHT,          \
-        _______, _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______, _______, _______, KC_END , KC_DOWN, KC_PGDN,          \
+        _______, _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______, _______, KC_HOME, KC_UP  , KC_PGUP, \
+        _______, KC_MUTE,  KC_VOLD,  KC_VOLU,  RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_SAI,  RGB_VAI,  RGB_SPI,            _______, _______, _______, KC_LEFT, _______, KC_RGHT,          \
+        _______, _______,  _______,  _______,  RGB_RMOD, RGB_HUD,  RGB_SAD,  RGB_VAD,  RGB_SPD,            _______,  _______, _______, _______, KC_END , KC_DOWN, KC_PGDN,          \
         _______, _______,                                _______,                                                   _______,  _______, _______, _______, _______, _______                                                                             \
     ),
 
