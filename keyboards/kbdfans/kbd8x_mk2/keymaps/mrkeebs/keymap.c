8f043490952770610286b7ad5ac8093338305e26
Felipe Coury <felipe.coury@gmail.com>
2020-02-07 04:01:02 -0300

    feat(kbdfans/kbd8x_mk2:mrkeebs): added layer toggle to Windows layout

--- keyboards/kbdfans/kbd8x_mk2/keymaps/mrkeebs/keymap.c
+++ keyboards/kbdfans/kbd8x_mk2/keymaps/mrkeebs/keymap.c
@@ -15,21 +15,41 @@
  */
 #include QMK_KEYBOARD_H
 
+// Layer shorthand
+enum layers {
+    _BASE = 0, // Base
+    _WIN,      // Windows
+    _CTRL      // Extras
+};
+
+#define KC_TWIN TG(_WIN)
+#define KC_MCTL MO(_CTRL)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT(
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10,  KC_F11,  KC_F12,                   KC_PSCR, KC_SLCK, KC_PAUS, \
-    KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,  KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL, KC_INS,  KC_HOME, KC_PGUP, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,  KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,         KC_DEL,  KC_END , KC_PGDN, \
-    KC_LCTL, KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,  KC_SCLN, KC_QUOT,     KC_ENT,                                          \
-    KC_LSFT, KC_BSLS, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT, KC_DEL,                   KC_UP,            \
-    KC_LALT, KC_LCTL, KC_LGUI,                 KC_SPC,                                KC_RGUI, KC_RALT, MO(1),   MO(1),           KC_LEFT, KC_DOWN, KC_RGHT  \
+  [_BASE] = LAYOUT(
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    KC_PSCR, KC_SLCK, KC_PAUS, \
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  KC_INS,  KC_HOME, KC_PGUP, \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_DEL,  KC_END , KC_PGDN, \
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                                               \
+    KC_LSFT, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_DEL,                    KC_UP,            \
+    KC_LALT, KC_LCTL, KC_LGUI,                   KC_SPC,                                      KC_RGUI, KC_RALT, KC_MCTL, KC_MCTL,          KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
-  [1] = LAYOUT(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ ,_______, _______, \
-    _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______, _______ ,_______ ,_______, \
-    _______, KC_MUTE, KC_VOLD, _______, KC_VOLU, _______, _______, _______, _______, _______, _______,     _______,                                          \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          \
-    _______, _______, _______,                 _______,                              _______, _______, _______, _______,          _______, _______, _______  \
+
+  [_WIN] = LAYOUT(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ ,_______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______ ,_______ ,_______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                                              \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,          \
+    KC_LCTL, KC_LGUI, KC_LALT,                   _______,                                     KC_RGUI, KC_RGUI, _______, _______,          _______, _______, _______  \
+  ),
+
+  [_CTRL] = LAYOUT(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, RESET,            _______, _______ ,_______, \
+    _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______, _______, _______, _______, _______, _______,                                              \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,          \
+    _______, _______, _______,                   _______,                                     KC_TWIN, KC_TWIN, _______, _______,          _______, _______, _______  \
   )
 };
