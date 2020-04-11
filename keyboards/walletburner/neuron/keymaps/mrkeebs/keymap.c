02ea7ab40514bcb60f92cd5bde3c5a7d9176aeb0
Felipe Coury <felipe.coury@gmail.com>
2020-04-11 19:56:41 -0300

    feat(neuron:mrkeebs): added Windows toggle layer

--- keyboards/walletburner/neuron/keymaps/mrkeebs/keymap.c
+++ keyboards/walletburner/neuron/keymaps/mrkeebs/keymap.c
@@ -3,6 +3,7 @@
 // Layer shorthand
 enum layers {
     _BASE = 0, // Base
+		_WIN,
     _EXTR,     // Extra
     _CTRL      // Control
 };
@@ -10,7 +11,7 @@ enum layers {
 #define KC_CTAB MT(MOD_LCTL, KC_TAB)  // Control held, Tab pressed
 #define KC_EXSP LT(_EXTR, KC_SPC)     // Extras layer held, Space pressed
 #define KC_CTSL LT(_CTRL, KC_SLSH)    // Control layer held, Slash pressed
-#define RGB_MSW RGB_M_SW              // Changes to Swirl underglow mode
+#define KC_TWIN TG(_WIN)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -18,7 +19,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
 		KC_CTAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_ENT,
 		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_CTSL,
-		KC_RALT, KC_LGUI, KC_EXSP, KC_SPC, KC_SPC, KC_RGUI, KC_RALT),
+		KC_RALT, KC_LGUI, KC_EXSP, KC_SPC, KC_SPC, KC_RGUI, KC_RCTL),
+
+	[_WIN] = LAYOUT(
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		KC_LGUI, KC_RALT, _______, _______, _______, KC_RALT, _______),
 
 	[_EXTR] = LAYOUT(
 		KC_GRV,  KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______,
@@ -30,6 +37,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		RESET, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END , KC_UP, _______, _______,
 		_______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, KC_PGUP, KC_LEFT, KC_RGHT, _______,
 		_______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_DOWN, _______, _______,
-		_______, _______, _______, _______, _______, _______, _______)
+		_______, _______, _______, _______, _______, KC_TWIN, _______)
 };
 
