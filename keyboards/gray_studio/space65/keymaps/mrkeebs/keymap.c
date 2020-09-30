eb9ac7133f36026dfa169e6748845d3999981264
Felipe Coury <felipe.coury@gmail.com>
2020-09-30 13:34:20 -0300

    feat(space65:mrkeebs): added VIA to Space65

--- keyboards/gray_studio/space65/keymaps/mrkeebs/keymap.c
+++ keyboards/gray_studio/space65/keymaps/mrkeebs/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,          KC_PGUP, \
         KC_CTCP, KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_ENT,           KC_PGDN, \
         KC_LSFT, KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,            KC_LEND, \
-        KC_LALT, KC_LALT, KC_LGUI,                KC_SPC, KC_SPC, KC_SPC,         KC_RGUI, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT                    \
+        KC_LCTL, KC_LALT, KC_LGUI,                KC_SPC, KC_SPC, KC_SPC,         KC_RGUI, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT                    \
     ),
 [_WIN] = LAYOUT_ISO( \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
@@ -42,9 +42,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LGUI, KC_LALT, _______, _______, _______, KC_RALT, KC_RALT, _______, _______, _______                    \
     ),
 [_CTRL] = LAYOUT_ISO( \
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, _______, _______, _______, _______, \
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, _______, _______, KC_DEL, KC_DEL, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,   _______, _______, \
-        KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        KC_CAPS, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  \
         _______, _______, _______, _______, _______, _______, KC_TWIN, KC_TWIN, KC_TWIN, _______, _______                    \
     ),
