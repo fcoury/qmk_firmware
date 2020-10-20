d3b3adc102f8fec1ad32d0d49d02124098ef40f7
Felipe Coury <felipe.coury@gmail.com>
2020-10-19 21:22:41 -0300

    feat(equinox:mrkeebs): win/mac fixes and via

--- keyboards/ai03/equinox/keymaps/mrkeebs/keymap.c
+++ keyboards/ai03/equinox/keymaps/mrkeebs/keymap.c
@@ -33,13 +33,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_CTAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
     KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_CTSL,
-    KC_LCTL, KC_LALT, KC_LGUI,          KC_EXSP,          KC_EXSP,      KC_SPC,      KC_TWIN, KC_RALT, KC_RCTL
+    KC_LALT, KC_LALT, KC_LGUI,          KC_EXSP,          KC_EXSP,      KC_SPC,      KC_TWIN, KC_RALT, KC_RCTL
   ),
   [_WIN] = LAYOUT_all( /* Windows Mods */
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, KC_LGUI, KC_LALT,          _______,          _______,     _______,      _______, _______, _______
+    KC_LGUI, KC_LGUI, KC_LALT,          _______,          _______,     _______,      _______, _______, _______
   ),
   [_EXTR] = LAYOUT_all( /* Extra Keys */
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
